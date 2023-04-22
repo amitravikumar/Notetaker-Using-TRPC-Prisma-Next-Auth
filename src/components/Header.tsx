8a9b23ca73057487cc531cc1abfeadb73190a593
Amit Kumar <43449014+amitravikumar@users.noreply.github.com>
2023-04-22 22:28:10 +0530

    Updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,6 +8,31 @@ export const Header = () => {
       <div className="flex-1 pl-5 text-3xl font-bold">
         {sessionData?.user?.name ? `Notes for ${sessionData.user.name}` : ""}
       </div>
+      <div className="flex-none gap-2">
+        <div className="dropdown-end dropdown">
+          {sessionData?.user ? (
+            <label
+              tabIndex={0}
+              className="btn-ghost btn-circle avatar btn"
+              onClick={() => void signOut()}
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  src={sessionData?.user?.image ?? ""}
+                  alt={sessionData?.user?.name ?? ""}
+                />
+              </div>
+            </label>
+          ) : (
+            <button
+              className="btn-ghost rounded-btn btn"
+              onClick={() => void signIn()}
+            >
+              Sign in
+            </button>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
