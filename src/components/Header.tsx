6d96391cf8166be582a12f7e36de1e9f458e5262
Amit Kumar <43449014+amitravikumar@users.noreply.github.com>
2023-04-22 22:06:53 +0530

    Added Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+
+export const Header = () => {
+  const { data: sessionData } = useSession();
+
+  return (
+    <div className="navbar bg-primary text-primary-content">
+      <div className="flex-1 pl-5 text-3xl font-bold">
+        {sessionData?.user?.name ? `Notes for ${sessionData.user.name}` : ""}
+      </div>
+    </div>
+  );
+};
