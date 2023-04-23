79a08afaf84ccb81f6453271435c86dee29e68c9
Amit Kumar <amitkumar@Amits-MacBook-Air.local>
2023-04-23 22:01:23 +0530

    Fixes

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -50,8 +50,8 @@ export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
-      clientId: env.GITHUB_CLIENT_ID,
-      clientSecret: env.GITHUB_CLIENT_SECRET,
+      clientId: env.GITHUB_CLIENT_ID_PROD,
+      clientSecret: env.GITHUB_CLIENT_SECRET_PROD,
     }),
     /**
      * ...add more providers here
